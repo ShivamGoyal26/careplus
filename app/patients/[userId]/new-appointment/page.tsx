d23e43b4a9d4c32aae6eea6b05d536ade6d94f51
shivamgoyal26 <officialshivamgoyal@gmail.com>
2024-07-28 14:16:47 +0530

    Page and form layout added

--- app/patients/[userId]/new-appointment/page.tsx
+++ app/patients/[userId]/new-appointment/page.tsx
@@ -1,5 +1,38 @@
-const NewAppointment = () => {
-  return <div>New Appointment</div>;
-};
+import Image from "next/image";
 
-export default NewAppointment;
+// Files
+import PatientForm from "@/components/forms/PatientForm";
+import Link from "next/link";
+import AppointmentForm from "@/components/forms/AppointmentForm";
+
+export default function NewAppointment() {
+  return (
+    <div className="flex h-screen max-h-screen">
+      <section className="remove-scrollbar container my-auto">
+        <div className="sub-container max-w-[860px] flex-1 justify-between">
+          <Image
+            src={"/assets/icons/logo-full.svg"}
+            height={1000}
+            width={1000}
+            alt="logo"
+            className="mb-12 h-10 w-fit"
+          />
+
+          <AppointmentForm />
+
+          <p className="justify-items-end text-dark-600 xl:text-left">
+            © 2024 CarePlus
+          </p>
+        </div>
+      </section>
+
+      <Image
+        src={"/assets/images/appointment-img.png"}
+        height={1000}
+        width={1000}
+        alt="appointment"
+        className="side-img max-w-[390px] bg-bottom"
+      />
+    </div>
+  );
+}
